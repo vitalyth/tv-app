9d7787b944a39382f989cd9e737073d3995790f4
Vitaly Thirulnikov <vitalyth@gmail.com>
2026-05-18 13:10:00 -0400

    fix: correct CACHE_DIR path in config.py

--- backend/config.py
+++ backend/config.py
@@ -3,4 +3,4 @@ from pathlib import Path
 
 APP_VERSION = "0.1.2"
 BASE_DIR = Path(__file__).resolve().parent
-CACHE_DIR = Path(os.getenv("BACKEND_CACHE_DIR", BASE_DIR.parent / "cache"))
+CACHE_DIR = Path(os.getenv("BACKEND_CACHE_DIR", BASE_DIR / "cache"))
